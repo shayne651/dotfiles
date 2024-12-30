45b1eddfcecede02b74a8bdb41d033ae963a6b17
Shayne Taylor <shaynetaylor@Shaynes-MacBook-Pro.local>
2024-12-29 20:04:51 -0500

    ignoring bruno files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function ()
+        require('telescope').setup({
+          defaults = {
+            file_ignore_patterns = { "./bruno/.*"},
+          },
+        })
+      end
     }
